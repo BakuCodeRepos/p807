917c2dfe3da759f149374b66387321ef69ca4691
Isa Arifoglu <arifogluisa@gmail.com>
2023-12-25 23:37:11 +0400

    bug fixed

--- SalesProject/settings.py
+++ SalesProject/settings.py
@@ -167,9 +167,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
